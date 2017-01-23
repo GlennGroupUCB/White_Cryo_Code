8ea4ed11f4d6824185bfa04408faa90e862f5eba
Wheeler1711 <Wheeler1711@gmail.com>
2017-01-22 18:16:06 -0700

    Added a bunch of comments to temp_monitor

--- Temp_monitor.py
+++ Temp_monitor.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import numpy as np
 import visa
 import matplotlib
-matplotlib.use('qt4agg')
+matplotlib.use('qt4agg')#need for continuously updating plot
 import matplotlib.pyplot as plt
 import time
 import datetime
@@ -17,6 +17,7 @@ import matplotlib.gridspec as gridspec
 
 #Change log
 #12/9/16 - Jordan - Added reading and ploting of the voltage and currents of the power supplies.
+#1/22/16 - Jordan - Added a bunch of comment to clarify the code
 
 #To Do 
 # need to modularize the reading of the temperatures to something like the reading of the power supplies. 
@@ -24,15 +25,19 @@ import matplotlib.gridspec as gridspec
 #
 #Need to add printing of the voltages and current to the file as well
 
-RX202_lookup = np.loadtxt('RX-202A Mean Curve.tbl')#202 ADR sensor
+RX202_lookup = np.loadtxt('RX-202A Mean Curve.tbl')#202 ADR sensor look up table
 #RX202_lookup = np.loadtxt('RX-102A Mean Curve.tbl') #102 300mK/ 1K sensors
-RX202_interp = interpolate.interp1d(RX202_lookup[:,1], RX202_lookup[:,0],fill_value = 0.,bounds_error = False)
+RX202_interp = interpolate.interp1d(RX202_lookup[:,1], RX202_lookup[:,0],fill_value = 0.,bounds_error = False) # interpolates the temperature when in between lookup table values
 #test = np.float(RX202_interp(4000))
 #RX202_temps = RX202_interp(-linear_bridge*1000)
 
+# this is just some stuff i use to cycle thorugh plot line styles/colors
 lines = ['-','--','-.']
-labels = ['4K P.T.               ','50K HTS            ','50K P.T.             ','50K plate          ','ADR rad shield','4He pump         ','3He pump         ','4He switch        ','3He switch        ','ADR switch        ','4K-1K switch      ','4K plate             ','3He head           ','4He head           ','4K HTS               ','ADR                    ','Head ADR switch']
 colors = ['b','g','r','c','m','y','k']
+
+#labels for the thermometers extra space is so the numbers all line up. might be a better way to add the space
+labels = ['4K P.T.               ','50K HTS            ','50K P.T.             ','50K plate          ','ADR rad shield','4He pump         ','3He pump         ','4He switch        ','3He switch        ','ADR switch        ','4K-1K switch      ','4K plate             ','3He head           ','4He head           ','4K HTS               ','ADR                    ','Head ADR switch']
+
 
 #allows you to not plot ugly thermometers
 plots = (0,1,2,3,5,6,7,8,9,10,11,12,13,14,15,16)
@@ -47,22 +52,23 @@ Alarm = 1 # 0 for off 1 for on
 
 now = datetime.datetime.now()
 date_str = str(now)[0:10]
+# we want the file prefix to reflect the date in which the temperature data is taken
 file_prefix =  "C:/Users/tycho/Desktop/White_Cryo_Code/Temps/" + date_str
 file_suffix = ''
 
 plt.figure(1,figsize = (21,11))
-ax = plt.gca()
-x = np.arange(-420,0)*1.
+ax = plt.gca() #need for changing legend labels
+x = np.arange(-420,0)*1. # initalize the x axis i.e. time going 420 mins into the past
 print(x[0],x[419])
-y = np.ones((420,17))*-1
-volt_y = np.zeros((420,6))
-curr_y = np.zeros((420,6))
+y = np.ones((420,17))*-1 #initalize array to hold temperautere
+volt_y = np.zeros((420,6)) #initialize array to hold voltages
+curr_y = np.zeros((420,6)) #initialize array to hold currents
 
 plt.title("Thermometry")
 plt.xlabel("time (mins)")
 plt.ylabel("Temperature (K)")
-plt.ion()
-gs = gridspec.GridSpec(3,3)#allows for custome subplot layout
+plt.ion() #need for constantly updating plot
+gs = gridspec.GridSpec(3,3)#allows for custom subplot layout
 plt.show()
 
 
@@ -72,39 +78,40 @@ rm = visa.ResourceManager()
 print, rm.list_resources()
 
 #form connections to the two lakeshore temperature sensors available
-lk224 = rm.open_resource('GPIB0::12::INSTR')
-lk218 = rm.open_resource('GPIB0::2::INSTR')
-lr750 = rm.open_resource('GPIB0::4::INSTR')
+lk224 = rm.open_resource('GPIB0::12::INSTR') #lakeshore 224
+lk218 = rm.open_resource('GPIB0::2::INSTR') #lakeshore 218
+lr750 = rm.open_resource('GPIB0::4::INSTR') #linear bridge
 
 #double check that you've connected to the lakeshore temperature sensors by asking them their names
 print(lk218.query('*IDN?'))
 print(lk224.query('*IDN?'))
 
 start = time.time() #define a start time
-if os.path.isfile(file_prefix + '_temps.txt') == True:
-	file_suffix = '_'+str(datetime.datetime.now())[11:13]+'_'+str(datetime.datetime.now())[14:16]
+if os.path.isfile(file_prefix + '_temps.txt') == True: #check if there is already a file with the prefix we are trying to use
+	#If a file already exists add a suffix to the end that consist of the time it was created so it can be distinguished from the original file
+	file_suffix = '_'+str(datetime.datetime.now())[11:13]+'_'+str(datetime.datetime.now())[14:16] 
 f = open(file_prefix + file_suffix +'_temps.txt' ,'w') #open a file to write the temperatures to
-i = 0
+i = 0 #initialize a counter
 try: #allows you to kill the loop with ctrl c
-	while True: #count the seconds since the start time, and sleep for 1 second between each count
-		now2 = time.time()
+	while True: #Just loop for every never stopping the monitoring of the temperatures
+		now2 = time.time() #do I need this still
 		now = datetime.datetime.now()
-		if str(now)[0:10] != date_str: #if it is a new day start a new file
-			f.close()
+		if str(now)[0:10] != date_str: #checks if the day has changed (i.e.) at midnight,if it is a new day start a new file
+			f.close() #close the old file
 			date_str = str(now)[0:10]
 			file_prefix =  "C:/Users/tycho/Desktop/White_Cryo_Code/Temps/" + date_str
 			file_suffix = ''
-			f = open(file_prefix + file_suffix +'_temps.txt' ,'w') #open a file to write the temperatures to
+			f = open(file_prefix + file_suffix +'_temps.txt' ,'w') #open a new file to write the temperatures to
 			
-		t = time.time()-start
-		x = np.roll(x,-1)
-		x[419] = t/60.
-		y = np.roll(y,-1,axis = 0)
-		volt_y = np.roll(volt_y,-1,axis = 0)
-		curr_y = np.roll(curr_y,-1,axis = 0)
+		t = time.time()-start #current time
+		x = np.roll(x,-1) #shift the time array by 1 interval
+		x[419] = t/60. #record current time to last position of time array
+		y = np.roll(y,-1,axis = 0) # shift temperature array by 1 interval
+		volt_y = np.roll(volt_y,-1,axis = 0) #shift voltage array by 1 interval
+		curr_y = np.roll(curr_y,-1,axis = 0) #shift current array by 1 interval
 
-			
-		y[419,0] = lk218_T1 = float(lk218.query('KRDG?1'))
+		#grab temperatures and store them to the temperature array	
+		y[419,0] = lk218_T1 = float(lk218.query('KRDG?1')) 
 		#print(y[419,0],lk218_T1)
 		y[419,14] = lk218_T2 = float(lk218.query('KRDG?2'))
 		y[419,1] = lk218_T3 = float(lk218.query('KRDG?3'))
@@ -123,28 +130,28 @@ try: #allows you to kill the loop with ctrl c
 		y[419,12] = lk224_A = float(lk224.query('KRDG? A'))
 		y[419,13] = lk224_B = float(lk224.query('KRDG? B'))
 		y[419,16] = lk224_TD1 = float(lk224.query('KRDG? D1'))
-		lr750_a = lr750.query('GET 0')
+		lr750_a = lr750.query('GET 0') #grab resitance from linear bridge
 		#print(lr750_a)
 		if i == 0: # there is some weirdness where the first call returns an empty string
 			lr750_a_temp = -1
 		if i != 0:
 			try: #every once in a while this fails
-				lr750_a_num = np.float(lr750_a[0:8])
+				lr750_a_num = np.float(lr750_a[0:8]) #convert resitance string to float
 				print(lr750_a_num)
-				y[419,15] = lr750_a_temp = RX202_interp(-lr750_a_num*1000)
+				y[419,15] = lr750_a_temp = RX202_interp(-lr750_a_num*1000) # convert restance value to temperature
 			except:
-				y[419,15] = lr750_a_temp = -1.
+				y[419,15] = lr750_a_temp = -1. #if we get a bad string just return -1
 		
-		k = 0
+		k = 0 #intiialize a counter
 		
-		plt.subplot(gs[0:2,:])
-		for j in plots:
+		plt.subplot(gs[0:2,:]) #creat top subplot 
+		for j in plots: #plot all of the temperatures with appropriate labels
 			#print(y[419,j])
 			plt.semilogy(x,y[:,j],color = colors[np.mod(j,7)],linestyle = lines[j/7],linewidth = 2, label = labels[j]+" " +str(y[419,j])[0:5]+"K")
 			if i != 0:
-				legend.get_texts()[k].set_text(labels[j]+" " +str(y[419,j])[0:5]+"K")
+				legend.get_texts()[k].set_text(labels[j]+" " +str(y[419,j])[0:5]+"K") #if it is not the first time ploting rewrite the legend with the new temps
 			k = k+1
-		if i == 0:					
+		if i == 0:	#if it is the first time ploting generate the legend				
 			legend = plt.legend(ncol = 1,loc =2)
 		plt.xlim(x[0],x[419])
 		plt.ylim(0.1,300)
@@ -160,21 +167,21 @@ try: #allows you to kill the loop with ctrl c
 		for j in range(0,len(volt)):
 			plt.plot(x,volt_y[:,j],color = colors[np.mod(j,7)],linestyle = lines[j/7],linewidth = 2, label = power_labels[j]+ " " +str(volt_y[419,j])[0:4]+"V " + str(curr_y[419,j])[0:6]+"A")
 			if i != 0:
-				legend_power.get_texts()[k].set_text(power_labels[j]+" " +str(volt_y[419,j])[0:4]+"V "+ str(curr_y[419,j])[0:6]+"A")
+				legend_power.get_texts()[k].set_text(power_labels[j]+" " +str(volt_y[419,j])[0:4]+"V "+ str(curr_y[419,j])[0:6]+"A")#if it is not the first time ploting rewrite the legend with the new temps
 			k = k+1
 		if i ==0:
-			legend_power= plt.legend(ncol = 1,loc = 2)		
+			legend_power= plt.legend(ncol = 1,loc = 2)	 #If it is the first time plotting generate the legend	
 		plt.xlim(x[0],x[419])
 		plt.ylim(0,30)
 			
-		plt.draw()
+		plt.draw() #update the plot
 
-		
-		if Alarm != 0:
+		#Alarm function
+		if Alarm != 0: #if the alarm is turned on proceed
 			Alarm_test = y[419,:]*Alarm_on #0 if not on otherwise actual temperature
-			if (Alarm_test>(Alarm_value +.001)).any() == True:
+			if (Alarm_test>(Alarm_value +.001)).any() == True: #have any of the alarm values been reached
 				print("Alarm Alarm")
-				if Alarm == 1:
+				if Alarm == 1: # we only want it to send us texts once not over and over again
 					print("first occurance")
 					fromaddr = 'SubmmLab@gmail.com'
 					toaddrs  = '3145741711@txt.att.net'
@@ -185,30 +192,29 @@ try: #allows you to kill the loop with ctrl c
 					server.ehlo()
 					#server.starttls()
 					server.login(username,password)
-					server.sendmail(fromaddr, toaddrs, msg)
+					server.sendmail(fromaddr, toaddrs, msg) #send a text
 					toaddrs  = '3038192582@tmomail.net'
 					server.sendmail(fromaddr, toaddrs, msg)
 					toaddrs  = '5308487272@pm.sprint.com'
 					server.sendmail(fromaddr, toaddrs, msg)
 					server.quit()	
-				Alarm = 2
-		
-		
+				Alarm = 2 #Alarm on but already triggered
 				
-		if i == 0:
+				
+		if i == 0: #if it is the first time writing to file put in a header
+			# not sure this header is up to date also need to add header if a new day has started
 			print('#Human readable time. Time (s) since start. Lakeshore temperature sensor 218 T1,3,5,6,8 and 224 C2,C3,C4,C5,D2,D3,D5,A,B',file=f)
+		# write to file
 		print(str(now)+' '+ str(np.round(t,3)).strip()+' '+str(lk218_T1)+' '+str(lk218_T3)+' '+str(lk218_T5)+' '+str(lk218_T6)+' '+str(lk218_T8)+' '+str(lk224_TC2)+' '+str(lk224_TC3)+' '+str(lk224_TC4)+' '+str(lk224_TC5)+' '+str(lk224_TD2)+' '+str(lk224_TD3)+' '+str(lk224_TD5)+' '+str(lk224_A)+' '+str(lk224_B)+' '+str(lk218_T2)+' '+str(lr750_a_temp)+' '+str(lk224_TD1),file = f) #print the temperature and some nonsense numbers to the file
+		# write to command prompt
 		print(str(now)+' '+ str(np.round(t,3)).strip()+' '+str(lk218_T1)+' '+str(lk218_T3)+' '+str(lk218_T5)+' '+str(lk218_T6)+' '+str(lk218_T8)+' '+str(lk224_TC2)+' '+str(lk224_TC3)+' '+str(lk224_TC4)+' '+str(lk224_TC5)+' '+str(lk224_TD2)+' '+str(lk224_TD3)+' '+str(lk224_TD5)+' '+str(lk224_A)+' '+str(lk224_B)+' '+str(lk218_T2)+' '+str(lr750_a_temp)+' '+str(lk224_TD1))
 		#time.sleep(sleep_interval)#sleep for 60 second
-		plt.pause(sleep_interval)
+		plt.pause(sleep_interval) # pause for sleep interval before looping again
 		
-		i = i + 1
+		i = i + 1 #icrement the counter
+				
 		
-							
-		
-		
-		
-except KeyboardInterrupt:
+except KeyboardInterrupt: #if you press ctrl c quit
     pass
 
 f.close() #close the file
