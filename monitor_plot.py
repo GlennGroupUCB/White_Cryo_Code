be622e1de96dbbb6626d2eb5cbcff7b208814917
Wheeler1711 <Wheeler1711@gmail.com>
2019-03-05 16:08:34 -0700

    added in functionality to plot magnet current for use with ADR
    I added in the ability to plot the magnet current for using this
    plotting program with ADR_4K_sink.py.

--- monitor_plot.py
+++ monitor_plot.py
@@ -10,6 +10,10 @@ from matplotlib import gridspec
 # Revision History:
 #   5/29/18 - Sean - Initial version
 #   5/31/18 - Sean - Made plotting the pressure optional so it can be used in fridge_cycle.py without any outside changes
+#   2/20/19 - Jordan - Added in optional plotting of magnet current for calling with ADR cycle
+
+# To Do:
+# - currently there is a problem with the xlim when intializing with negative time
 
 class MonitorPlot:
 	"""
@@ -63,7 +67,7 @@ class MonitorPlot:
 		return self._plot_pressure
 
 
-	def __init__(self, sleep_interval, hist_size, plot_pressure=True):
+	def __init__(self, sleep_interval, hist_size, plot_pressure=True,plot_magnet = False):
 		"""
 		Creates and opens the new plot with default data, ready for updating in the future.
 		
@@ -76,6 +80,7 @@ class MonitorPlot:
 		self._sleep_minutes = sleep_interval / 60
 		self._hist_size = hist_size = int(hist_size)
 		self._plot_pressure = plot_pressure = bool(plot_pressure)
+		self._plot_magnet = plot_magnet = bool(plot_magnet)
 
 		# Set up the data history arrays
 		self._time_data = np.arange(-hist_size, 0) * self._sleep_minutes
@@ -83,13 +88,21 @@ class MonitorPlot:
 		self.V_data = np.zeros((hist_size, 6))
 		self.A_data = np.zeros((hist_size, 6))
 		self.P_data = np.zeros((hist_size, 1)) if plot_pressure else None
+		self.M_data = np.zeros((hist_size, 1)) if plot_magnet else None
 
 		# Prepare the figure
 		self._fig = plt.figure(figsize=(21, 11))
-		pgrid = gridspec.GridSpec(4 if plot_pressure else 3, 1)
+		if plot_pressure:
+			pgrid = gridspec.GridSpec(4, 1)
+		elif plot_magnet:
+			pgrid = gridspec.GridSpec(4, 1)
+		else:
+			pgrid = gridspec.GridSpec(3, 1)
+			
 		self._T_plot = self._fig.add_subplot(pgrid[0:2, 0])
 		self._V_plot = self._fig.add_subplot(pgrid[(2, 0)], sharex=self._T_plot)
 		self._P_plot = self._fig.add_subplot(pgrid[(3, 0)], sharex=self._T_plot) if plot_pressure else None
+		self._M_plot = self._fig.add_subplot(pgrid[(3, 0)], sharex=self._T_plot) if plot_magnet else None
 		self._fig.subplots_adjust(hspace=0)
 
 		# Style the plots
@@ -103,6 +116,11 @@ class MonitorPlot:
 			self._P_plot.set_xlabel('Time (mins)')
 			self._P_plot.set_ylim((0.0001, 1300.0))
 			self._P_plot.set_xlabel('Time Since Start (mins)')
+		elif plot_magnet:
+			self._M_plot.set_ylabel('Magnet Current (A)')
+			self._M_plot.set_xlabel('Time (mins)')
+			self._M_plot.set_ylim((-1, 11))
+			self._M_plot.set_xlabel('Time Since Start (mins)')
 		else:
 			self._V_plot.set_xlabel('Time Since Start (mins)')
 
@@ -119,15 +137,23 @@ class MonitorPlot:
 			self._P_plot.semilogy(self._time_data, self.P_data[:, i], label=('Pressure {:>7.3f} mbar').format(self.P_data[:,i][-1]))
 			for i in range(1)
 		] if plot_pressure else None
+		
+		self._M_lines = [
+			self._M_plot.plot(self._time_data, self.M_data[:, i], label=('Current {:>7.3f} A').format(self.M_data[:,i][-1]))
+			for i in range(1)
+		] if plot_magnet else None
 
 		# Create and cache the legends
-		self._T_leg = self._T_plot.legend(loc='upper left')
+		self._T_leg = self._T_plot.legend(loc='upper left',fontsize = 11)
 		self._V_leg = self._V_plot.legend(loc='upper left')
 		self._P_leg = self._P_plot.legend(loc='upper left') if plot_pressure else None
+		self._M_leg = self._M_plot.legend(loc='upper left') if plot_magnet else None
 		plt.setp(self._T_leg.texts, family='consolas')
 		plt.setp(self._V_leg.texts, family='consolas')
 		if plot_pressure:
 			plt.setp(self._P_leg.texts, family='consolas')
+		if plot_magnet:
+			plt.setp(self._V_leg.texts, family='consolas')
 
 
 	def show(self):
@@ -138,7 +164,7 @@ class MonitorPlot:
 		plt.show()
 
 
-	def update(self, time, Ts, Vs, As, Ps, redraw=True):
+	def update(self, time, Ts, Vs, As, Ps, Ms = [0],redraw=True):
 		"""
 		Update the plot with the data from the new sample.
 		
@@ -156,6 +182,8 @@ class MonitorPlot:
 		self.A_data = np.roll(self.A_data, -1, axis=0)
 		if self._plot_pressure:
 			self.P_data = np.roll(self.P_data, -1, axis=0)
+		if self._plot_magnet:
+			self.M_data = np.roll(self.M_data, -1, axis=0)
 
 		# Put the new data in the history arrays
 		self._time_data[-1] = time / 60
@@ -164,7 +192,9 @@ class MonitorPlot:
 		self.A_data[-1,:] = As
 		if self._plot_pressure:
 			self.P_data[-1,:] = Ps
-
+		if self._plot_magnet:
+			self.M_data[-1,:] = Ms
+			
 		# Update the temperature lines
 		for i, t_line in enumerate(self._T_lines):
 			t_line[0].set_data(self._time_data, self.T_data[:, i])
@@ -181,12 +211,20 @@ class MonitorPlot:
 				p_line[0].set_data(self._time_data, self.P_data[:, i])
 				self._P_leg.texts[i].set_text(('Pressure {:>7.3f} mbar').format(self.P_data[:, i][-1]))
 
+		if self._plot_magnet:
+			for i, m_line in enumerate(self._M_lines):
+				m_line[0].set_data(self._time_data, self.M_data[:, i])
+				self._M_leg.texts[i].set_text(('Current {:>7.3f} A').format(self.M_data[:, i][-1]))
+				
+				
 		# Redraw the plot if requested
 		if redraw:
 			self._T_plot.set_xlim(self._time_data[0], self._time_data[-1] + self._sleep_minutes * 2)
-			time_str = 'Current Time  -  {:0>2d}:{:0>2d}:{:0>6.3f}'.format(int(time // 3600), int(time // 60), time % 60)
+			time_str = 'Current Time  -  {:0>2d}:{:0>2d}:{:0>6.3f}'.format(int(time // 3600), np.mod(int(time // 60),60), time % 60)
 			if self._plot_pressure:
 				self._P_plot.set_xlabel('Time Since Start (mins)\n\n{}'.format(time_str))
+			elif self._plot_magnet:
+				self._M_plot.set_xlabel('Time Since Start (mins)\n\n{}'.format(time_str))
 			else:
 				self._V_plot.set_xlabel('Time Since Start (mins)\n\n{}'.format(time_str))
 			self._fig.canvas.draw_idle()
